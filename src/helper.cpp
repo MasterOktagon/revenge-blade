b4234e24cad8816d1e8ce3fa543a459e68f6f0b1
MasterOktagon <userkontakt.hexagongames@gmail.com>
2024-09-03 23:41:58 +0200

    FIX: missing constructor for vector2d

--- src/helper.cpp
+++ src/helper.cpp
@@ -1,10 +1,15 @@
-template <typename T>
 
-class Vector2D {
-public:
-  T x;
-  T y;
-  Vector2D operator+(Vector2D v2) { return Vector2D(x + v2.x, y + v2.y); }
-  Vector2D operator-(Vector2D v2) { return Vector2D(x - v2.x, y - v2.y); }
-  T operator*(Vector2D v2) { return x * v2.x + y * v2.y; }
-};
+
+Vector2D::Vector2D(T x, T y){
+    this->x = x;
+    this->y = y;
+}
+Vector2D<T> Vector2D::operator+(Vector2D v2) {
+    return Vector2D(x + v2.x, y + v2.y);
+}
+Vector2D<T> Vector2D::operator-(Vector2D v2) {
+    return Vector2D(x - v2.x, y - v2.y);
+}
+T Vector2D::operator*(Vector2D v2) {
+    return x * v2.x + y * v2.y;
+}
--- src/helper.h
+++ src/helper.h
@@ -1,7 +1,11 @@
+#pragma once 
+
+
 template <typename T> class Vector2D {
     public:
         T x;
         T y;
+        Vector2D (T x, T y);
         Vector2D operator+(Vector2D v2);
         Vector2D operator-(Vector2D v2);
         T operator*(Vector2D v2);
