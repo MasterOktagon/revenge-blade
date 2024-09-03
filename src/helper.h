1ffcf3f66fb053deb1bf040fc6405506b4d3ad47
MasterOktagon <userkontakt.hexagongames@gmail.com>
2024-09-03 23:38:01 +0200

    FIX: x,y was private

--- src/helper.h
+++ src/helper.h
@@ -1,7 +1,8 @@
 template <typename T> class Vector2D {
-  T x;
-  T y;
-  Vector2D operator+(Vector2D v2);
-  Vector2D operator-(Vector2D v2);
-  T operator*(Vector2D v2);
+    public:
+        T x;
+        T y;
+        Vector2D operator+(Vector2D v2);
+        Vector2D operator-(Vector2D v2);
+        T operator*(Vector2D v2);
 };
