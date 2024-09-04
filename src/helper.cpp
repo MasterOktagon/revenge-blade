94ab47b0d4258cf675261b4d91299e324fe79b2a
Ranastra <101139864+Ranastra@users.noreply.github.com>
2024-09-04 13:29:00 +0200

    ADD: scalar mult for Vector2D

--- src/helper.cpp
+++ src/helper.cpp
@@ -14,5 +14,8 @@ template <typename T> Vector2D<T> Vector2D<T>::operator-(Vector2D v2) {
 template <typename T> T Vector2D<T>::operator*(Vector2D v2) {
   return x * v2.x + y * v2.y;
 }
+template <typename T> Vector2D<T> Vector2D<T>::operator*(T scalar) {
+  return Vector2D(x * scalar, y * scalar);
+}
 
 template class Vector2D<int32_t>; // doesnt work without this, idk why
